d138361ace194b918e2521ca3591dee9de7b299b
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2019-04-12 09:46:22 -0300

    Ajustes no projeto para funcionar no Wildfly 16

--- src/main/java/org/otojunior/springbootwildfly/SpringbootWildflyApplication.java
+++ src/main/java/org/otojunior/springbootwildfly/SpringbootWildflyApplication.java
@@ -4,16 +4,45 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 
+ * @author Oto Soares Coelho Junior
+ *
+ */
 @SpringBootApplication
 public class SpringbootWildflyApplication extends SpringBootServletInitializer {
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringbootWildflyApplication.class);
-    }
-	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootWildflyApplication.class);
+	}
+
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootWildflyApplication.class, args);
 	}
-
+	
+	/**
+	 * 
+	 * @author Oto Soares Coelho Junior
+	 */
+	@RestController
+	class SpringbootWildflyHomeController {
+		/**
+		 * 
+		 * @return
+		 */
+		@RequestMapping("/")
+		public String inicio() {
+			return "<h3>SpringbootWildflyApplication online!</h3>";
+		}
+	}
 }
